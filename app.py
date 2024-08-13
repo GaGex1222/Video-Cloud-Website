de674fe71dda61671d2317dd1e2d317dc3042b80
Gal <gald12123434@gmail.com>
2024-08-14 02:11:52 +0300

    Tweaked app completely to meet heroku requirements

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-from flask import Flask, abort, render_template, redirect, url_for, flash, request, session, send_file
+from flask import Flask, render_template, redirect, url_for, flash, request, send_file
 from flask_bootstrap import Bootstrap4
 from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import Integer, String, LargeBinary
+from sqlalchemy import  String
 from sqlalchemy.exc import IntegrityError
 from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.utils import secure_filename
 from form import UploadingVideo, LoginForm, Register
 import os
-from io import BytesIO
-from flask_mail import Mail, Message
+
 
 class Base(DeclarativeBase):
     pass
@@ -21,7 +21,11 @@ def load_user(user_id):
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('DATABASE_URL_REAL')
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('DATABASE_URL')
+UPLOAD_FOLDER = 'static'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+ 
+
 
 db = SQLAlchemy(app, model_class=Base)
 login_manager.init_app(app)
@@ -36,13 +40,20 @@ class Uploads(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     person: Mapped[str] = mapped_column(String)
     filename: Mapped[str] = mapped_column(String)
-    data: Mapped[bytes] = mapped_column(LargeBinary, unique=True)
+    file_path: Mapped[str] = mapped_column(String)
 
 with app.app_context():
     db.create_all()
 
 def is_admin():
     return current_user.username == 'Faceit'
+
+allowed_ext = ['mp4', 'mov', 'mkv', 'avi']
+
+def check_for_video_file_extensions(filename):
+    if '.' in filename:
+        return filename.rsplit('.')[1] in allowed_ext
+    return False
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
@@ -56,14 +67,18 @@ def home():
     if request.method == 'POST':
         file_uploaded = request.files['video_file']
         person_name = request.form['name']
-        upload = Uploads(
-            filename=file_uploaded.filename,
-            person=person_name,
-            data=file_uploaded.read()
-        )
-        db.session.add(upload)
-        db.session.commit()
-        return f'Uploaded {file_uploaded.filename}'
+        if file_uploaded:
+            filename = secure_filename(file_uploaded.filename)
+            file_uploaded.save(os.path.join('static', filename))
+            upload = Uploads(
+                filename=filename,
+                person=person_name.lower(),
+                file_path=f'static/{filename}'
+            )
+            db.session.add(upload)
+            db.session.commit()
+            error = f'Successfully uploaded {filename} to faceit storage'
+            return render_template('error.html', error=error)
 
     return render_template('index.html', form=form, current_user=current_user)
 
@@ -77,12 +92,15 @@ def files():
 
     if request.method == 'POST':
         query = request.form.get('query').lower()
+        print(query)
         specific_files = db.session.execute(db.select(Uploads).where(Uploads.person == query)).fetchall()
+        print(specific_files)
         files_list = [f[0] for f in specific_files]
         return render_template('files.html', files=files_list)
 
     files = db.session.execute(db.select(Uploads)).fetchall()
     files_list = [f[0] for f in files]
+    print(files_list)
     return render_template('files.html', files=files_list)
 
 @app.route('/register', methods=['POST', 'GET'])
@@ -113,9 +131,11 @@ def download(file_id):
 
     requested_file = db.session.execute(db.select(Uploads).where(Uploads.id == file_id)).scalar()
     if requested_file:
-        return send_file(BytesIO(requested_file.data), download_name=requested_file.filename, as_attachment=True)
+        return send_file(requested_file.file_path)
+    else:
+        return render_template('error.html', error='Cant find file in database')
 
-    return render_template('error.html', error='File not found')
+
 
 @app.route('/preview/<file_id>')
 def preview(file_id):
@@ -123,10 +143,17 @@ def preview(file_id):
         return render_template('error.html', error='You have to be logged in as admin to use this page')
 
     requested_file = db.session.execute(db.select(Uploads).where(Uploads.id == file_id)).scalar()
-    if requested_file:
-        return send_file(BytesIO(requested_file.data), as_attachment=False, download_name=requested_file.filename)
+    if check_for_video_file_extensions(requested_file.filename):
+        if requested_file:
+            print('previewing file')
+            return render_template('preview.html', requested_file=requested_file)
+        else:
+            error = 'No File Detected, tell gal'
+            return render_template('error.html', error=error)
+    else:
+        error = 'File selected is not a video'
+        return render_template('error.html', error=error)
 
-    return render_template('error.html', error='File not found')
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
@@ -146,6 +173,19 @@ def login():
         return redirect(url_for('login'))
 
     return render_template('login.html', form=form)
+
+@app.route('/delete/<file_id>', methods=['POST', 'GET'])
+def delete(file_id):
+    requested_video_query = db.session.execute(db.select(Uploads).where(Uploads.id == file_id)).scalar()
+    if requested_video_query:
+        db.session.delete(requested_video_query)
+        db.session.commit()
+        os.remove(requested_video_query.file_path)
+        current_query_params = request.args.to_dict()
+        return redirect(url_for('files', **current_query_params))
+    else:
+        return render_template('error.html', error=f'Couldnt find file with id of {file_id}, talk to gal')
+
 
 @app.route('/logout')
 def logout():
